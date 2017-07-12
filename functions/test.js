214a500fc17cd97f4003a88de2646fa61f4ef117
Aaron Turner <aaron@aaronthedev.com>
2017-07-12 16:46:38 -0700

    Made a fun little  firebase function

--- functions/test.js
+++ functions/test.js
@@ -3,5 +3,6 @@ const functions = require('firebase-functions');
 
 // The function we are exporting
 module.exports = functions.https.onRequest((request, response) => {
-  response.send(JSON.stringify(request, null, 4));
+  const responseString = "IP: " + request.ip + ", Query Strings: " + JSON.stringify(request.query, null, 4);
+  response.send(responseString);
 });
