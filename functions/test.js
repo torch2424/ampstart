cae598aaf003f61f758621e808620b839c0f9779
Aaron Turner <aaron@aaronthedev.com>
2017-07-12 17:33:07 -0700

    Got a func working little firebase function

--- functions/test.js
+++ functions/test.js
@@ -1,8 +1,22 @@
 // Our require statements
 const functions = require('firebase-functions');
+const prettyBytes = require('pretty-bytes');
+
 
 // The function we are exporting
 module.exports = functions.https.onRequest((request, response) => {
-  const responseString = "IP: " + request.ip + ", Query Strings: " + JSON.stringify(request.query, null, 4);
-  response.send(responseString);
+  let responseString = "IP: " + request.ip + ", Query Strings: ";
+  Object.keys(request.query).forEach(function(key) {
+    responseString += key+ ": " + request.query[key] + ", ";
+  });
+
+  let prettyBytesString = '';
+  if (request.query.bytes && /^\d+$/.test(request.query.bytes)) {
+    prettyBytesString += prettyBytes(parseInt(request.query.bytes, 10));
+  }
+
+  response.json({
+      responseString: responseString,
+      prettyBytes: prettyBytesString
+  });
 });
